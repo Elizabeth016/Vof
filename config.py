38a7bfe8450d7a8ac2b599aa41c3f9f465ee2ebf
Elizabeth016 <kunelizabeth625@gmail.com>
2024-12-17 22:38:15 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY @SomsPvt
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7193823078:AAGPGBbwbxZ9LPdd3zWjGV0QQ-D9Vfb1dfM")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@BLURYFAXE")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1020771953")
+
+
+
+
+
+
+
+  
